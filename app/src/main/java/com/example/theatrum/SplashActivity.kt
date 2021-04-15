7b3561250ade0f0545cdb79a8b22194666c5baee
mikhaeapg <mikhaelapg00@gmail.com>
2021-04-15 23:31:17 +0700

    view pager remove

--- app/src/main/java/com/example/theatrum/SplashActivity.kt
+++ app/src/main/java/com/example/theatrum/SplashActivity.kt
@@ -3,27 +3,18 @@ package com.example.theatrum
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.example.theatrum.util.SharedPrefereces
 
 class SplashActivity : AppCompatActivity() {
-    lateinit var pre: SharedPrefereces
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        pre = SharedPrefereces(this)
 
         Handler().postDelayed({
             var i = Intent()
-
-            if (pre.firstInstall == false) {
-                i = Intent(this, WalkthroughActivity::class.java)
-            } else {
-                i = Intent(this, MainActivity::class.java)
-            }
+            i = Intent(this, MainActivity::class.java)
 
             startActivity(i)
             finish()
