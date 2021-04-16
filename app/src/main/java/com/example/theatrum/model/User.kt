039e2db7393fbf9a4a15710545a9643467f21ca0
mikhaeapg <mikhaelapg00@gmail.com>
2021-04-16 21:24:39 +0700

    coba google auth

--- app/src/main/java/com/example/theatrum/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.theatrum.model
-
-// model class
-data class User(
-        val id: Int = -1,
-        val name: String,
-        val email: String,
-        val password: String
-)
--- app/src/main/java/com/example/theatrum/sql/DatabaseHelper.kt
+++
@@ -1,4 +0,0 @@
-package com.example.theatrum.sql
-
-class DatabaseHelper {
-}
