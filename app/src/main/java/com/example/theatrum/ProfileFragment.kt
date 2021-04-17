af98cd3a8604562f276d14e6baebff38e463e726
mikhaeapg <mikhaelapg00@gmail.com>
2021-04-17 20:18:20 +0700

    upload photo

--- app/src/main/java/com/example/theatrum/ProfileFragment.kt
+++ app/src/main/java/com/example/theatrum/ProfileFragment.kt
@@ -1,15 +1,27 @@
 package com.example.theatrum
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_profile.*
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
+
+    companion object {
+        const val REQUEST_CAMERA = 100
+    }
+
+    private lateinit var imageUri : Uri
 
     private  lateinit var auth: FirebaseAuth
 
@@ -30,6 +42,46 @@ class ProfileFragment : Fragment() {
                 startActivity(it)
             }
         }
+
+        ivProfile.setOnClickListener {
+            intentCamera()
+        }
+    }
+
+    private fun intentCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {intent->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also {
+                    startActivityForResult(intent, REQUEST_CAMERA)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImage(imgBitmap)
+        }
+    }
+
+    private fun uploadImage(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("img/${FirebaseAuth.getInstance().currentUser?.uid}")
+
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val image = baos.toByteArray()
+
+        ref.putBytes(image)
+                .addOnCompleteListener {
+                    ref.downloadUrl.addOnCompleteListener {
+                        it.result?.let {
+                            imageUri = it
+                            ivProfile.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
     }
 
 }
